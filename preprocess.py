cd589eb5e669a759c0f98966919a3f6c5de1ae29
Shaw Wong <ShawWong@yeah.net>
2023-04-05 20:11:48 +0800

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,140 @@
+import csv
+import random
+import numpy as np
+
+
+class Preprocess:
+    def __init__(self, mask_id: int = 9, mask_prob: float = 0.15,
+                 input_prefix=None, output_prefix=None, trans_prefix=None,
+                 input_len: int = 160, output_len: int = 150,
+                 pad_id: int = 0, sos_id: int = 1, eos_id: int = 2):
+        if input_prefix is None:
+            input_prefix = [1] * 10  # 预测输入mask的prefix
+        if output_prefix is None:
+            output_prefix = [2] * 10  # 预测输出mask的prefix
+        if trans_prefix is None:
+            trans_prefix = [3] * 10  # 翻译任务的prefix
+        self.__mask_id = mask_id
+        self.__mask_prob = mask_prob
+        self.__input_prefix = input_prefix
+        self.__output_prefix = output_prefix
+        self.__trans_prefix = trans_prefix
+        self.__input_len = input_len
+        self.__output_len = output_len
+        self.__pad_id = pad_id
+        self.__sos_id = sos_id
+        self.__eos_id = eos_id
+
+    def for_finetune(self, data_file: str, shuffle=False):
+        with open(data_file, 'r') as fp:
+            reader = csv.reader(fp)
+            samples = [row for row in reader]
+        fp.close()
+        data_trans = []
+        for sample in samples:
+            source = [int(x) for x in sample[1].split()]
+            for prefix in self.__trans_prefix:
+                source.insert(0, prefix)
+            if len(source) < self.__input_len:
+                source.extend([self.__pad_id] * (self.__input_len - len(source)))
+            target = [self.__sos_id] + [int(x) for x in sample[2].split()] + [self.__eos_id]
+            if len(target) < self.__output_len:
+                target.extend([self.__pad_id] * (self.__output_len - len(target)))
+            data_trans.append([source, target])
+        return np.array(data_trans, dtype=object)
+
+    def for_pretrain(self, data_file: str, shuffle=False):
+        with open(data_file, 'r') as fp:
+            reader = csv.reader(fp)
+            samples = [row for row in reader]
+        fp.close()
+        data_input, data_output = [], []
+        for sample in samples:
+            for _ in range(5):  # random mask five times
+                input_words = [int(x) for x in sample[1].split()]
+                data_input.append(self.__random_mask(input_words))
+                if len(sample) == 3:  # if there are output pretrain, random mask five times
+                    output_words = [int(x) for x in sample[2].split()]
+                    data_output.append(self.__random_mask(output_words))
+        if shuffle:
+            random.shuffle(data_input)
+            random.shuffle(data_output)
+        for ele in data_input:  # handle input pretrain
+            for prefix in self.__input_prefix:
+                ele[0].insert(0, prefix)  # add input task prefix
+            if len(ele[0]) < self.__input_len:  # add pad_id
+                ele[0].extend([self.__pad_id] * (self.__input_len - len(ele[0])))
+            if len(ele[1]) < self.__output_len:  # add pad_id
+                ele[1].extend([self.__pad_id] * (self.__output_len - len(ele[1])))
+            if len(ele[2]) < self.__output_len:  # add pad_id
+                ele[2].extend([self.__pad_id] * (self.__output_len - len(ele[2])))
+        for ele in data_output:  # handle output pretrain
+            for prefix in self.__output_prefix:
+                ele[0].insert(0, prefix)  # add output task prefix
+            if len(ele[0]) < self.__input_len:  # add pad_id
+                ele[0].extend([self.__pad_id] * (self.__input_len - len(ele[0])))
+            # ele[1] = [self.sos_id] + ele[1] + [self.eos_id]  # add sos_id, eos_id
+            if len(ele[1]) < self.__output_len:  # add pad_id
+                ele[1].extend([self.__pad_id] * (self.__output_len - len(ele[1])))
+            if len(ele[2]) < self.__output_len:  # add pad_id
+                ele[2].extend([self.__pad_id] * (self.__output_len - len(ele[2])))
+        if len(data_output) == 0:
+            return np.array(data_input, dtype=object)
+        return np.array(data_input, dtype=object), np.array(data_output, dtype=object)
+
+    def __random_mask(self, words: list):
+        # obviously, if there's only one word, don't mask it
+        if len(words) == 1:
+            return words
+        # randomly mask words with probability p
+        mask_words = []  # record word after mask
+        mask_label = []  # record ground truth
+        mask_pos = []  # record mask position
+        mask_num = 0
+        for word in words:
+            r = random.uniform(0, 1)
+            if r < self.__mask_prob:
+                mask_words.append(self.__mask_id)
+                mask_label.append(word)
+                mask_pos.append(1)
+                mask_num += 1
+            else:
+                mask_words.append(word)
+                mask_label.append(0)
+                mask_pos.append(0)
+        if mask_num == 0:  # mask one word at least!!!
+            index = random.randint(0, len(mask_words) - 1)
+            mask_label[index] = mask_words[index]
+            mask_words[index] = self.__mask_id
+            mask_pos[index] = 1
+        return [mask_words, mask_label, mask_pos]
+
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    current_work_dir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(current_work_dir)  # change work dir to current work dir
+    Path('data/pretrain').mkdir(exist_ok=True, parents=True)
+    pre = Preprocess()
+    (data_in, data_out) = pre.for_pretrain("data/train.csv")
+    # for idx in range(5):
+    #     print("mask input with mask token, mask position: ",
+    #           data_in[idx][0], data_in[idx][1], data_in[idx][2], sep='\n')
+    #     print("mask output with mask token, mask position: ",
+    #           data_out[idx][0], data_out[idx][1], data_in[idx][2], sep='\n')
+    print("input pretrain shape: ", data_in.shape, "output pretrain shape: ", data_out.shape)
+    np.save('data/pretrain/data_in', data_in)
+    np.save('data/pretrain/data_out', data_out)
+    data_in = Preprocess().for_pretrain('data/test.csv')
+    # for idx in range(5):
+    #     print("mask with unmask: ", data_in[idx][0], data_in[idx][1], sep='\n')
+    print("test input pretrain shape: ", data_in.shape)
+    np.save('data/pretrain/data_in_test', data_in)
+    Path('data/finetune').mkdir(exist_ok=True, parents=True)
+    data_trans = pre.for_finetune("data/train.csv")
+    # for idx in range(5):
+    #     print("trans: ", data_trans[idx][0], data_trans[idx][1], sep='\n')
+    print("trans finetune shape: ", data_trans.shape)
+    np.save('data/finetune/data_trans', data_trans)
---
+++ pretrain.py
@@ -0,0 +1,155 @@
+import os
+import time
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from dataset import PretrainDataset
+from model import Transformer
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3"
+
+
+class Logger:
+    def __init__(self, file_name, mode="w", buffer=100):
+        Path(file_name).parent.mkdir(exist_ok=True, parents=True)
+        self.file_name = file_name
+        self.fp = open(file_name, mode)
+        self.cnt = 0
+        self.stamp = time.time()
+        self.buffer = buffer
+
+    def log(self, *args, end="\n"):
+        for x in args:
+            if isinstance(x, dict):
+                for y in x:
+                    self.fp.write(str(y) + ":" + str(x[y]) + " ")
+            else:
+                self.fp.write(str(x) + " ")
+        self.fp.write(end)
+        self.cnt += 1
+        if self.cnt >= self.buffer or time.time() - self.stamp > 5:
+            self.cnt = 0
+            self.stamp = time.time()
+            self.fp.close()
+            self.fp = open(self.file_name, "a")
+
+    def close(self):
+        self.fp.close()
+
+
+class Checkpoint:
+    def __init__(self, model):
+        self.model = model
+
+    def load(self, model_path):
+        memory = torch.load(model_path)
+        if isinstance(self.model, torch.nn.DataParallel):
+            self.model.module.load_state_dict(memory)
+        else:
+            self.model.load_state_dict(memory)
+
+    def save(self, save_path):
+        if isinstance(self.model, torch.nn.DataParallel):
+            ret = self.model.module.state_dict()
+        else:
+            ret = self.model.state_dict()
+        torch.save(ret, save_path)
+
+
+class Config(dict):
+    def __init__(self, version):
+        super().__init__()
+        self['version'] = version
+        self['device'] = 'cuda:0'
+        self['multi_train'] = True
+        if self['multi_train']:
+            self['device_ids'] = [0, 1, 2, 3]
+            self['batch_size'] = 64 * len(self['device_ids'])
+        else:
+            self['batch_size'] = 32
+        self['prefix_len'] = 10
+        self['input_data'] = "data/pretrain/data_in.npy"
+        self['test_input_data'] = "data/pretrain/data_in_test.npy"
+        self['output_data'] = "data/pretrain/data_out.npy"
+        self['input_len'] = 160
+        self['output_len'] = 150
+        self['n_token'] = 2000
+        self['n_layer'] = 6
+        self['sos_id'] = 1
+        self['eos_id'] = 2
+        self['pad_id'] = 0
+        self['lr'] = 1e-3
+        self['pretrain_model'] = None
+        self['start_epoch'] = 0
+        self['n_epoch'] = self['start_epoch'] + 50
+        self['model_dir'] = 'checkpoint/pretrain/%d' % self['version']
+
+
+def train(config):
+    train_data = PretrainDataset(config['input_data'], config['test_input_data'], config['output_data'])
+    train_loader = DataLoader(train_data, batch_size=config['batch_size'], shuffle=True, drop_last=False)
+
+    pretrain = Transformer(
+        input_l=config['input_len'],
+        output_l=config['output_len'],
+        n_token=config['n_token'],
+        encoder_layer=config['n_layer'],
+        decoder_layer=config['n_layer'],
+        sos_id=config['sos_id'],
+        pad_id=config['pad_id']
+    ).to(config['device'])
+    checkpoint = Checkpoint(pretrain)
+    if config['multi_train']:
+        pretrain = torch.nn.DataParallel(pretrain, device_ids=config["device_ids"])
+    if config['pretrain_model'] is not None:
+        checkpoint.load(config['pretrain_model'])
+        print("load pretrain model successfully!")
+
+    optim = Adam([{'params': pretrain.parameters(), 'initial_lr': 1e-3}], lr=config['lr'])
+    lr_schedule = StepLR(optim, 5, 0.9, last_epoch=config['start_epoch'])
+    loss_func = CrossEntropyLoss(ignore_index=0).to(config['device'])
+
+    Path(config["model_dir"]).mkdir(exist_ok=True, parents=True)
+    writer = SummaryWriter(config["model_dir"])
+    logger = Logger(config["model_dir"] + "/log.txt", "w")
+    logger.log(config)
+
+    best_loss = np.inf
+    for epoch in range(config["start_epoch"], config["n_epoch"]):  # train
+        losses = []
+        process_bar = tqdm(train_loader)
+        for idx, (source, target, position) in enumerate(process_bar):
+            source, target, position = source.to(config['device']), target.to(config['device']), \
+                position.to(config['device'])
+            pred = pretrain(source, source[:, config['prefix_len']:]) * position.unsqueeze(dim=-1)
+            pred = pred.reshape(-1, pred.shape[-1])
+            target = target.reshape(-1)
+            loss = loss_func(pred, target.long())
+            optim.zero_grad()  # 清空梯度
+            loss.backward()
+            optim.step()  # 优化一次
+            lr_schedule.step()
+            losses.append(loss.item())
+            process_bar.set_postfix(
+                epoch=epoch, lr="%.3f" % lr_schedule.get_last_lr()[-1], loss="%.3f" % float(np.mean(losses))
+            )
+        mean_loss = np.mean(losses)
+        logger.log("epoch: %d:" % epoch, "mean loss: %f of " % mean_loss, losses)
+        writer.add_scalar("batch loss", mean_loss, epoch)
+        if mean_loss < best_loss:
+            best_loss = mean_loss
+            checkpoint.save(config["model_dir"] + "/model_{}_{}.pt".format(epoch, best_loss))
+    writer.close()
+    logger.close()
+
+
+if __name__ == '__main__':
+    train(Config(version=1))
